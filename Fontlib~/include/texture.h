2dc4ddc06f5a29cac93b10b446d8c13c7c663aed
Yonder <yondercode@gmail.com>
2025-04-11 22:59:56 +0700

    Fix support for glyphs larger than the em square

--- Fontlib~/include/texture.h
+++ Fontlib~/include/texture.h
@@ -8,6 +8,7 @@
 #include <msdfgen.h>
 #include <msdfgen-ext.h>
 #include <mathematics.h>
+#include <math.h>
 #include <glyph.h>
 #include "clipper2/clipper.h"
 
@@ -269,33 +270,31 @@ namespace Text
         edgeColoringSimple(shape, 3.0);
 
         float scale = static_cast<float>(glyphSize);
-        int bitmapSize = glyphSize + padding * 2;
 
         // Get glyph bounds from Shape (normalized 0-1 units)
         msdfgen::Shape::Bounds bounds = shape.getBounds();
-        float glyphLeft = bounds.l;
-        float glyphBottom = bounds.b;
-        float translateX = (padding - glyphLeft * scale) / scale;
-        float translateY = (padding - glyphBottom * scale) / scale;
-
-        msdfgen::Vector2 translate(translateX, translateY);
-        msdfgen::Bitmap<float, 4> outBitmap(bitmapSize, bitmapSize);
+        float translate_x = (padding - bounds.l * scale) / scale;
+        float translate_y = (padding - bounds.b * scale) / scale;
+        msdfgen::Vector2 translate(translate_x, translate_y);
+        msdfgen::Bitmap<float, 4> tempBitmap(glyphMetrics.atlas_width_px, glyphMetrics.atlas_height_px);
         msdfgen::Projection projection = msdfgen::Projection(scale, translate);
         msdfgen::SDFTransformation transform(projection, msdfgen::Range(distanceMappingRange));
         msdfgen::MSDFGeneratorConfig config(true);
-        msdfgen::generateMTSDF(outBitmap, shape, transform, config);
+        msdfgen::generateMTSDF(tempBitmap, shape, transform, config);
+
+        // Copy from the temp bitmap to the atlas texture
         for (int dx = 0; dx < glyphMetrics.atlas_width_px; ++dx)
         {
             for (int dy = 0; dy < glyphMetrics.atlas_height_px; ++dy)
             {
-                int destX = glyphMetrics.atlas_x_px + dx;
-                int destY = glyphMetrics.atlas_y_px + dy;
-                int srcX = dx;
-                int srcY = dy;
-                if (destX >= 0 && destX < textureSize && destY >= 0 && destY < textureSize)
+                int dest_x = glyphMetrics.atlas_x_px + dx;
+                int dest_y = glyphMetrics.atlas_y_px + dy;
+                int src_x = dx;
+                int src_y = dy;
+                if (dest_x >= 0 && dest_x < textureSize && dest_y >= 0 && dest_y < textureSize)
                 {
-                    auto dest = refTexture->Data() + destY * textureSize + destX;
-                    auto src = outBitmap(srcX, srcY);
+                    auto dest = refTexture->Data() + dest_y * textureSize + dest_x;
+                    auto src = tempBitmap(src_x, src_y);
                     dest->r = pixelFloatToByte(src[0]);
                     dest->g = pixelFloatToByte(src[1]);
                     dest->b = pixelFloatToByte(src[2]);
